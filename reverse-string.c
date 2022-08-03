f98678d7fb1dff47f3b8d504c9103dacf83ebd10
Kazem Latifi <kazem.latifi76@gmail.com>
2022-08-03 16:40:49 +0430

    my finally commit

--- reverse-string.c
+++ reverse-string.c
@@ -2,18 +2,16 @@
 #include <stdlib.h>
 #include "_string.h"
 
-
-
-
 int main()
 {
     char *name = (char *)malloc(200 * sizeof(char));
-    printf("What's your string?\n");
-    int size = _strlen(name);
-    _strreverse(name);
-    printf("Length of name is %d\n", size);
 
-    printf("reverse name is %s\n", name);
+    printf("What's your string?\n");
+    _strreverse(name);
+
+    printf("Length of name is : %d\n", _strlen(name));
+
+    printf("reverse name is : %s\n", name);
+    
     return 0;
 }
-
